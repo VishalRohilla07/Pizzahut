307cf780a03ee9d715d41d6f19ffc06fa5d6ab08
Vishal <rohillavishal12@gmail.com>
2021-12-25 10:59:37 +0530

    add orders table,place order

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,34 +1,46 @@
-import axios from 'axios'
-import Noty from 'noty'
+import axios from "axios";
+import Noty from "noty";
+import { initAdmin } from "./admin";
 
-let addToCart = document.querySelectorAll('.add-to-cart')
-let cartCounter = document.querySelector('#cartCounter')
+let addToCart = document.querySelectorAll(".add-to-cart");
+let cartCounter = document.querySelector("#cartCounter");
 
-function updateCart(pizza){
-    axios.post('/update-cart',pizza).then(res =>{
-        console.log(res);
-        cartCounter.innerText = res.data.totalQty
-        new Noty({
-            type: 'success',
-            timeout: 1000,
-            text: 'Item added to cart',
-            progressBar: false,
-        }).show();
-    }).catch(err => {
-        new Noty({
-            type: 'error',
-            timeout: 1000,
-            text: 'something went wrong',
-            progressBar:false
-        }).show();
-
+function updateCart(pizza) {
+  axios
+    .post("/update-cart", pizza)
+    .then((res) => {
+      console.log(res);
+      cartCounter.innerText = res.data.totalQty;
+      new Noty({
+        type: "success",
+        timeout: 1000,
+        text: "Item added to cart",
+        progressBar: false,
+      }).show();
     })
+    .catch((err) => {
+      new Noty({
+        type: "error",
+        timeout: 1000,
+        text: "something went wrong",
+        progressBar: false,
+      }).show();
+    });
 }
 
+addToCart.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    let pizza = JSON.parse(btn.dataset.pizza);
+    updateCart(pizza);
+  });
+});
 
-addToCart.forEach((btn) =>{
-    btn.addEventListener('click',(e) => {
-        let pizza = JSON.parse(btn.dataset.pizza)
-        updateCart(pizza)
-    })
-})
+/// Remove alert message after X seconds
+const alertMsg = document.querySelector("#success-alert");
+if (alertMsg) {
+  setTimeout(() => {
+    alertMsg.remove();
+  }, 2000);
+}
+
+initAdmin();
--- routes/web.js
+++ routes/web.js
@@ -1,7 +1,16 @@
 const authController = require('../app/http/controllers/authController')
 const cartController = require('../app/http/controllers/customers/cartController')
+const orderController = require('../app/http/controllers/customers/orderController')
 const homeController = require('../app/http/controllers/homeController')
+
+const AdminOrderController = require('../app/http/controllers/admin/orderController')
+
+//Middlewares
 const guest = require('../app/http/middlewares/guest')
+const auth = require('../app/http/middlewares/auth')
+const admin = require('../app/http/middlewares/admin')
+
+
 
 function initRoutes(app){
     app.get('/',homeController().index)
@@ -13,6 +22,14 @@ function initRoutes(app){
 
     app.get('/cart',cartController().index)
     app.post('/update-cart',cartController().update)
+
+    //Customer routes
+    app.post('/orders', auth ,orderController().store)
+    app.get('/customer/orders',auth,orderController().index)
+
+    // Admin routes 
+    app.get('/admin/orders',admin ,AdminOrderController().index)
+
 }
 
 
